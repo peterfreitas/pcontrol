aa5beaeab3c730acc6b06c6afc6418d5287527b9
Peter Freitas <freitaspeteroliveira@gmail.com>
2021-11-26 12:46:25 -0300

    Setup do projeto

---
+++ src/main/java/com/peterfreitas/pcontrol/PcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.peterfreitas.pcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peterfreitas/pcontrol/PcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterfreitas.pcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
