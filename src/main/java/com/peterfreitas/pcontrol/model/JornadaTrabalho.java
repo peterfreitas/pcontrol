b2c0eaae6adbf0064dcd46a0307ba01ebe403b52
Peter Freitas <freitaspeteroliveira@gmail.com>
2021-11-26 14:21:25 -0300

    Inclusão package model e Classe JornadaTrabalho

---
+++ src/main/java/com/peterfreitas/pcontrol/model/JornadaTrabalho.java
@@ -0,0 +1,16 @@
+package com.peterfreitas.pcontrol.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class JornadaTrabalho {
+
+    private long id;
+    private String descricao;
+    
+}
